399a269a8b3e9cac8bac856acb41138e273f175f
ysBach <ysbach93@gmail.com>
2026-05-07 06:51:08 +0900

    missing package imports

--- src/fitsmgmt/__init__.py
+++ src/fitsmgmt/__init__.py
@@ -3,40 +3,43 @@
 from .logging import logger, set_log_level, enable_console_logging
 from .airmass import *
 from .ccdutils import *
-from .filemgmt import *
 from .headers import *
 from .imstat import *
 from .io import *
 from .mathutils import *
 from .misc import *
+from .paths import *
 from .pixels import *
+from .summary import *
 from .wcstools import *
 
 from . import (
     airmass,
     ccdutils,
-    filemgmt,
     headers,
     imstat,
     io,
     logging,
     mathutils,
     misc,
+    paths,
     pixels,
+    summary,
     wcstools,
 )
 
 __all__ = [
     "airmass",
     "ccdutils",
-    "filemgmt",
     "headers",
     "imstat",
     "io",
     "logging",
     "mathutils",
     "misc",
+    "paths",
     "pixels",
+    "summary",
     "wcstools",
     "logger",
     "set_log_level",
@@ -45,13 +48,14 @@ __all__ = [
 for _module in (
     airmass,
     ccdutils,
-    filemgmt,
     headers,
     imstat,
     io,
     mathutils,
     misc,
+    paths,
     pixels,
+    summary,
     wcstools,
 ):
     __all__.extend(getattr(_module, "__all__", []))
